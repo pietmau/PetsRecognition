15a3f06948aa50d7c7bb2505300a190b8dfa7e45
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2023-10-01 14:06:54 +0100

    OK works

--- data/src/main/java/com/pietrantuono/data/networkchecker/NetworkCheckerImpl.kt
+++ data/src/main/java/com/pietrantuono/data/networkchecker/NetworkCheckerImpl.kt
@@ -1,16 +1,12 @@
 package com.pietrantuono.data.networkchecker
 
-import java.net.InetAddress
+import android.net.ConnectivityManager
 import javax.inject.Inject
 
 
-class NetworkCheckerImpl @Inject constructor() : NetworkChecker {
-    override suspend fun isNetworkAvailable() = try {
-        val inetAddress = InetAddress.getByName(GOOGLE)
-        !inetAddress.equals("")
-    } catch (e: Exception) {
-        false
-    }
+class NetworkCheckerImpl @Inject constructor(private val connectivityManager: ConnectivityManager) : NetworkChecker {
+    override suspend fun isNetworkAvailable() =
+        connectivityManager.activeNetwork != null && connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork) != null
 
     private companion object {
         private const val GOOGLE = "google.com"
