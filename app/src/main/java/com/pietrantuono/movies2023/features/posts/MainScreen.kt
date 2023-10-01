2996511f8181487b355c481c011dc1da5444f4be
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2023-10-01 20:31:00 +0100

    Better

--- app/src/main/java/com/pietrantuono/movies2023/features/posts/MainScreen.kt
+++ app/src/main/java/com/pietrantuono/movies2023/features/posts/MainScreen.kt
@@ -1,46 +1,29 @@
 package com.pietrantuono.movies2023.features.posts
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.pietrantuono.movies2023.features.posts.Action.NavigationPerformed
-import com.pietrantuono.movies2023.features.posts.UiState.Content
 import com.pietrantuono.movies2023.features.posts.ui.PostsScreen
 
 @Composable
 internal fun MainScreen() {
     val navController = rememberNavController()
     NavHost(
-        navController = navController,
-        startDestination = HOME
+        navController = navController, startDestination = HOME
     ) {
-        composable(HOME) {
-            val viewModel = hiltViewModel<MainViewModel>()
-            val viewState by viewModel.uiState.collectAsStateWithLifecycle(Content())
-            PostsScreen(viewState) { action ->
-                viewModel.accept(action)
-            }
-            LaunchedEffect(viewState.navDestination) {
-                navController.navigate(viewState.navDestination)
-                viewModel.accept(NavigationPerformed)
+        composable(route = HOME) {
+            PostsScreen {
+                navController.navigate(it)
             }
         }
         composable(
             route = "$DETAIL/{$NAME}",
             arguments = detailNavArguments
-        ) { backStackEntry ->
-            val name = backStackEntry.arguments?.getString(NAME)
-
+        ) {
+            val name = it.arguments?.getString(NAME)
+            Text(text = name ?: "No name")
         }
     }
 }
--- app/src/main/java/com/pietrantuono/movies2023/features/posts/ui/PostsScreen.kt
+++ app/src/main/java/com/pietrantuono/movies2023/features/posts/ui/PostsScreen.kt
@@ -1,13 +1,25 @@
 package com.pietrantuono.movies2023.features.posts.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.pietrantuono.movies2023.features.posts.Action
-import com.pietrantuono.movies2023.features.posts.UiState
+import com.pietrantuono.movies2023.features.posts.Destination
+import com.pietrantuono.movies2023.features.posts.MainViewModel
 import com.pietrantuono.movies2023.features.posts.UiState.Content
 
 @Composable
-fun PostsScreen(state: UiState, actions: (Action) -> Unit = {}) {
-    when (state) {
-        is Content -> Content(state, actions)
+fun PostsScreen(viewModel: MainViewModel = hiltViewModel(), navigateTo: (Destination) -> Unit = {}) {
+    val viewState by viewModel.uiState.collectAsStateWithLifecycle(Content())
+    when (val state = viewState) {
+        is Content -> Content(state) {
+            viewModel.accept(it)
+        }
+    }
+    LaunchedEffect(viewState.navDestination) {
+        navigateTo(viewState.navDestination)
+        viewModel.accept(Action.NavigationPerformed)
     }
 }
